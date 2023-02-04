cc62afd345fef48f3de7fe6fd3792f20d5385d69
zweix123 <1979803044@qq.com>
2023-02-04 21:08:55 +0800

    modify

--- source.js
+++ source.js
@@ -1,5 +1,5 @@
 // ==UserScript==
-// @name         B站合集总进度栏
+// @name         B站合集总进度条
 // @namespace    http://tampermonkey.net/
 // @version      0.1
 // @description  用于B站的合集在时间维度上的总进度，设计时尽可能解耦，利于个性化修改
@@ -14,21 +14,22 @@
 
 //javascript:( //function () {
 
-window.onload = function () { //入口
-    let number = 5;  //等待网页加载时间，这里选择5秒，是一个经验值，如果用起来不合适可以将这个延长
-    void function (time) {  //匿名的sleep函数，用于等网页加载完
+window.onload = function () {  // main
+    let number = 3.14;  // 等待网页加载时间, 是一个经验值, 如果不合适可以修改
+    void function (time) {  // 匿名的sleep函数，用于等网页加载完
         return new Promise((resolve) => setTimeout(resolve, time));
-    } (number * 1000).then(() => {  //等两秒
-        let list = document.getElementsByClassName('cur-page');  //获得分P栏的表头，如果不是合集其长度为0
+    } (number * 1000).then(() => {
+        let list = document.getElementsByClassName('cur-page');  // 获得分P栏的表头, 如果不是合集其长度为0, 一次判断是否运行程序
         if (list.length == 0) return ;
         solve();
     })
 }
-function get(num) {  //对于一个索引，得到从合集开始到这个索引视频的一些时间
+
+function get(num) {  // 对于一个索引, 得到从合集开始到这个索引视频的一些时间
     let hour = 0, minute = 0, second = 0;
     for (let i = 0; i < num; ++ i) {
         let vec = document
-            .getElementsByClassName('duration')[i]  //这个是每一个栏的信息
+            .getElementsByClassName('duration')[i]  // 这个是每一个栏的信息
             .innerHTML.match(/\d+/g);
         if (vec.length == 2) vec.unshift('0');
         hour += parseInt(vec[0]);
@@ -46,7 +47,7 @@ function get(num) {  //对于一个索引，得到从合集开始到这个索引
 }
 
 function solve() {
-    //分P栏的表头，利用正则表达式提取出想要的信息
+    // 分P栏的表头, 利用正则表达式提取出想要的信息
     let cur = parseInt(document
         .getElementsByClassName('cur-page')[0]
         .innerHTML.match(/(\d+)\//)[1]);
@@ -55,14 +56,14 @@ function solve() {
         .innerHTML.match(/\/(\d+)/)[1]);
     cur = get(cur)
     end = get(end)
-    //显示出来
+    // 显示出来
     show(cur, end)
 }
 
 function show(cur, end) {
-    let plain = document.getElementsByClassName('video-data')[0];  //确定位置
-    let sam = document.getElementById('button_tag_zweix');  //原因见下
-    let isNULL = (sam === null);  //因为这个地方要更新，这里的策略是如果没有就创建，没有就在其基础上修改
+    let plain = document.getElementsByClassName('video-data')[0];  // 确定位置
+    let sam = document.getElementById('button_tag_zweix');  // 原因见下
+    let isNULL = (sam === null);  // 因为这个地方要更新, 这里的策略是如果没有就创建, 没有就在其基础上修改
 
     if (isNULL) {
         sam = document.createElement('button');
@@ -76,14 +77,15 @@ function show(cur, end) {
             '    padding:0.5rem;\n' +
             '    cursor: pointer;\n' +
             '    ';
-        sam.addEventListener("click", solve);  //更新方案，手动点击按钮来更新，为何不采用更自动的方案看说明
+        sam.addEventListener("click", solve);  // 更新方案, 手动点击按钮来更新, 为何不采用更自动的方案见README
     }
 
-    sam.innerHTML = "现在是第" + cur.index + "集，共用时" + cur.str + ";  已完成" + (cur.sum / end.sum * 100).toFixed(2) + "%";
+    sam.innerHTML = "现在是第" + cur.index + "集, 共用时" + cur.str + "; 已完成" + (cur.sum / end.sum * 100).toFixed(2) + "%";
 
     if (isNULL) plain.appendChild(sam);
 
 }
+
 //})();
 
     // Your code here...
